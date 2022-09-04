28f3a9f6c20bf7b7f2867445a496e1214b28882f
realfelipe5 <krish2suisun@gmail.com>
2022-09-04 11:11:16 -0700

    favicon

---
+++ js/showcase.js
@@ -0,0 +1,91 @@
+
+function changeImage() {
+    var image = document.getElementById('display1');
+    if (image.src.match("img/gallery/display1.jpg")) {
+        image.src = "img/gallery/display1m.jpg";
+        image.style.setProperty("height", "320px", "important");
+    }
+    else {
+        image.src = "img/gallery/display1.jpg";
+    }
+}
+function changeImage2() {
+    var image = document.getElementById('display2');
+    if (image.src.match("img/gallery/display2.webp")) {
+        image.src = "img/gallery/display2m.jpg";
+        image.style.setProperty("height", "290px", "important");
+    }
+    else {
+        image.src = "img/gallery/display2.webp";
+    }
+}
+function changeImage3() {
+    var image = document.getElementById('display3');
+    if (image.src.match("img/gallery/display3.webp")) {
+        image.src = "img/gallery/display3m.jpg";
+        image.style.setProperty("height", "280px", "important");
+    }
+    else {
+        image.src = "img/gallery/display3.webp";
+    }
+}
+function changeImage4() {
+    var image = document.getElementById('display4');
+    if (image.src.match("img/gallery/display4.webp")) {
+        image.src = "img/gallery/display4m.jpg";
+        image.style.setProperty("height", "320px", "important");
+    }
+    else {
+        image.src = "img/gallery/display4.webp";
+    }
+}
+function changeImage5() {
+    var image = document.getElementById('display5');
+    if (image.src.match("img/gallery/display5.webp")) {
+        image.src = "img/gallery/display5m.jpeg";
+        image.style.setProperty("height", "320px", "important");
+    }
+    else {
+        image.src = "img/gallery/display5.webp";
+    }
+}
+function changeImage6() {
+    var image = document.getElementById('display6');
+    if (image.src.match("img/gallery/display6.webp")) {
+        image.src = "img/gallery/display6m.jpeg";
+        image.style.setProperty("height", "320px", "important");
+    }
+    else {
+        image.src = "img/gallery/display6.webp";
+    }
+}
+function changeImage7() {
+    var image = document.getElementById('display7');
+    if (image.src.match("img/gallery/display7.webp")) {
+        image.src = "img/gallery/display7m.jpg";
+        image.style.setProperty("height", "320px", "important");
+    }
+    else {
+        image.src = "img/gallery/display7.webp";
+    }
+}
+function changeImage8() {
+    var image = document.getElementById('display8');
+    if (image.src.match("img/gallery/display8.webp")) {
+        image.src = "img/gallery/display8m.jpg";
+        image.style.setProperty("height", "320px", "important");
+    }
+    else {
+        image.src = "img/gallery/display8.webp";
+    }
+}
+function changeImage9() {
+    var image = document.getElementById('display9');
+    if (image.src.match("img/gallery/display9.webp")) {
+        image.src = "img/gallery/display9m.jpg";
+        image.style.setProperty("height", "330px", "important");
+    }
+    else {
+        image.src = "img/gallery/display9.webp";
+    }
+}
--- nav.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.querySelector('nav');
-window.onscroll = () => {
-    if (window.scrollY > 610) {
-        navbar.classList.add('sch');
-    } else {
-        navbar.classList.remove('sch');
-    }
-};
