c5e699e72a626f6f51ce92f612d5ba1e27afc1d3
realfelipe5 <krish2suisun@gmail.com>
2022-09-09 21:53:46 -0700

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,1 @@
-function open() {
-    var win = window.open("", "win",
-                "width = 500, height = 300");
                  
-    win.document.write("<p>New Window</p>");
-     
-    win.opener.document.write("<p>Parent Window</p>");
-}
