40fc99022fd464a191e56e1f38abff2eb712f423
realfelipe5 <krish2suisun@gmail.com>
2022-09-11 19:41:36 -0700

    testsmooth

---
+++ js/smoothm.js
@@ -0,0 +1,13 @@
+// function myFunction() {
+//     var q = window.matchMedia("(max-width: 700px)")
+//     if (q.matches) { // If media query matches
+
+//         var mobile_Die = document.getElementById("mobile_Die")
+
+//         mobile_Die.remove();
+//     } else {
+//         document.getElementById('nerd').id = 'butter';
+//     }
+//   }
+
+// myFunction()
