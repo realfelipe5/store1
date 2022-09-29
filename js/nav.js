12fdefb4ee036db408160adba777e8a6d7efc662
realfelipe5 <krish2suisun@gmail.com>
2022-09-28 19:17:36 -0700

    final v2 bug fix

--- js/nav.js
+++ js/nav.js
@@ -3,9 +3,7 @@ const cart = document.getElementById('shopping');
 window.onscroll = () => {
     if (window.scrollY > 610) {
         navbar.classList.add('sch');
-        shopping.src = "img/b_cart.png";
     } else {
         navbar.classList.remove('sch');
-        shopping.src = "img/w_cart.png";
     }
 };
